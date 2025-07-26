3163e2cf0ae907699f7d1944650d037022f60659
Turbash Negi <negirawatdeepi@gmail.com>
2025-07-26 12:42:37 +0530

    Basic page done Added an image for page a font which looks like fountain pen but i am confused how do i generate love letter use ai and then do i need to make a backend cause i don't want to

---
+++ letterTemplates.js
@@ -0,0 +1,8 @@
+const letterTemplates = [
+  `To my dearest {to},\n\n`,
+  `From the moment I met you, my world changed.\n\n`,
+  `{message}\n\n`,
+  `With all my love,\n{from}`
+];
+
+export default letterTemplates;
