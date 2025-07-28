ea8639366caea9132a9f8c7cd8b7ceb7f1c5d01a
Turbash Negi <negirawatdeepi@gmail.com>
2025-07-28 15:18:42 +0530

    Preloading images for optimisation

--- letter-site/src/App.jsx
+++ letter-site/src/App.jsx
@@ -7,7 +7,7 @@ import HeartsBackground from './components/HeartsBackground';
 export default function App() {
   return (
     <div
-      className="min-h-screen h-full w-full flex flex-col items-center justify-center overflow-hidden bg-[#2d1a0d] bg-center bg-cover"
+      className="h-full w-full flex flex-col items-center justify-center overflow-hidden bg-[#2d1a0d] bg-center bg-cover"
       style={{ backgroundImage: "url('/wood-material-background-wallpaper-texture-concept.webp')" }}
     >
       <HeartsBackground />
